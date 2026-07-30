e3f94d5cc9d2744bcfbfa94cd763c257a0addf9a
Atti <atti.3064@outlook.com>
2026-07-30 08:01:02 +0800

    chore(repo): keep only core project files

--- src/main/resources/static/app/assets/esm-UFQmXj6b.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CNhYNOh1.js","assets/index-DKVS2fQD.js","assets/index-R0GU0pT4.css"])))=>i.map(i=>d[i]);
-import{r as e,t}from"./index-DKVS2fQD.js";var n;(function(e){e[e.Sunday=1]=`Sunday`,e[e.Monday=2]=`Monday`,e[e.Tuesday=3]=`Tuesday`,e[e.Wednesday=4]=`Wednesday`,e[e.Thursday=5]=`Thursday`,e[e.Friday=6]=`Friday`,e[e.Saturday=7]=`Saturday`})(n||={});var r=e(`LocalNotifications`,{web:()=>t(()=>import(`./web-CNhYNOh1.js`).then(e=>new e.LocalNotificationsWeb),__vite__mapDeps([0,1,2]))});export{r as LocalNotifications};
